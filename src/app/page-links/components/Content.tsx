69c8c62ad68aa285447fceaa52a46913b77fa087
ehdgur <ehdgur3506@naver.com>
2025-07-05 17:38:31 +0900

    link project ver 3.0

--- src/app/page-links/components/Content.tsx
+++ src/app/page-links/components/Content.tsx
@@ -1,20 +1,20 @@
 import AddLink from "@/app/page-links/components/AddLink";
 import LinkLists from "@/app/page-links/components/LinkLists";
-import NextPage from "./NextPage";
-import { getToLink } from "@/app/page-links/controllers/axiosLink";
-// import links from '@/app/api/page-links/links.json'
+// import NextPage from "./NextPage";
+// import { getToLink } from "@/app/page-links/controllers/axiosLink";
+import links from "@/app/api/page-links/links.json";
 
 export default async function Content() {
-  const links = await getToLink(1);
+  // const links = await getToLink(1);
 
   console.log(links);
   return (
     <div className="border-2">
       <div className="grid grid-cols-3 p-4 gap-4 rounded-2xl">
         <AddLink />
-        <LinkLists links={links.data} />
+        <LinkLists links={links} />
       </div>
-      <NextPage pages={links.totalPages} currentPage={links.page} />
+      {/* <NextPage pages={links.totalPages} currentPage={links.page} /> */}
     </div>
   );
 }
