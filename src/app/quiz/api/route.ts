0cdc94fd8d48cea0dec78aa5df64b4c1d12e5ed7
ehdgur <ehdgur3506@naver.com>
2025-09-30 22:02:25 +0900

    QUIZ AI ver 1.1

--- src/app/quiz/api/route.ts
+++ src/app/quiz/api/route.ts
@@ -6,6 +6,11 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// 문제의 보기를 랜덤으로 섞는 함수
+function shuffleArray<T>(array: T[]): T[] {
+  return [...array].sort(() => Math.random() - 0.5);
+}
+
 export async function POST(req: Request) {
   try {
     const { inputValue } = await req.json();
@@ -15,12 +20,23 @@ export async function POST(req: Request) {
         { status: 400 }
       );
     }
-
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
+    const content = `
+너는 ${inputValue} 전문가야. ${inputValue} 관련하여 객관식 퀴즈를 만들어줘.
+- 난이도는 초등학교 수준(1)부터 박사 수준(1000)까지로 하고, 내가 현재 원하는 난이도는 ${randomNumber}이야. 
+- 오답은 현실적이어야 하고, 답과 혼동될 수 있는 수준으로 만들어
+- 난이도는 explanation 마지막에 (난이도: 숫자) 형식으로 표시해
+- explanation의 글자 수는 150자 이내로 해. 
+- options를 만들 때, 번호는 생략해.
+`;
     const completion = await openai.responses.create({
-      model: "gpt-5-nano",
+      model: "gpt-5-mini",
       input: [
         { role: "system", content: "너는 객관식 퀴즈 생성기야." },
-        { role: "user", content: `${inputValue} 관련 퀴즈 1개 만들어줘` },
+        {
+          role: "user",
+          content: content,
+        },
       ],
       text: {
         format: {
@@ -37,7 +53,7 @@ export async function POST(req: Request) {
                 maxItems: 4,
               },
               answer: { type: "string" },
-              explanation: { type: "string", maxLength: 100 },
+              explanation: { type: "string", maxLength: 200 },
             },
             required: ["question", "options", "answer", "explanation"],
             additionalProperties: false,
@@ -46,9 +62,11 @@ export async function POST(req: Request) {
       },
     });
 
-    console.log(completion.output_text);
     const quiz = JSON.parse(completion.output_text);
-    return NextResponse.json(quiz);
+    const randomOptions = shuffleArray(quiz.options);
+    const convertQuiz = { ...quiz, options: randomOptions };
+    console.log(convertQuiz);
+    return NextResponse.json(convertQuiz);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,24 +1,46 @@
 "use client";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 import { postQuiz } from "./controllers/axiosQuiz";
+import useIsMobile from "@/app/hooks/useIsMobile";
 import { QuizResponse } from "./type";
 
-export default function ChatPage() {
-  const [response, setResponse] = useState<QuizResponse | null>(null);
+export default function QuizPage() {
   const [inputValue, setInputValue] = useState("");
   const [selectAnswer, setSelectAnswer] = useState("");
   const [resultMessage, setResultMessage] = useState("");
   const [isAnswer, setIsAnswer] = useState(false);
-  const sendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
+  const isMobile = useIsMobile();
+  const [loadingTime, setLoadingTime] = useState<number | null>(null);
+  const [response, setResponse] = useState<QuizResponse | null>(null);
+
+  // mutation 훅 정의
+  const quizMutation = useMutation({
+    mutationFn: (value: string) => postQuiz(value),
+  });
+
+  const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    try {
-      const res = await postQuiz(inputValue);
-      setResponse(res); // 서버가 이미 파싱해서 준 객체
-      console.log(res);
-    } catch (error) {
-      console.error(error);
-      setResponse(null);
-    }
+    setSelectAnswer("");
+    setResultMessage("");
+    setIsAnswer(false);
+    setResponse(null);
+    setLoadingTime(null);
+    const start = performance.now(); // 요청 시작 시점
+
+    quizMutation.mutate(inputValue, {
+      onSuccess: (data) => {
+        const end = performance.now();
+        setLoadingTime(end - start); // API 응답 시간
+        setResponse(data);
+      },
+      onError: (err) => {
+        const end = performance.now();
+        setLoadingTime(end - start);
+        console.error(err);
+        setResponse(null);
+      },
+    });
   };
 
   const checkAnswerClick = () => {
@@ -32,8 +54,16 @@ export default function ChatPage() {
   };
 
   return (
-    <div className="flex w-1/2 gap-4 flex-col justify-center items-center mx-auto">
-      <h1 className="text-6xl p-5 mt-10">QUIZ 생성기</h1>
+    <div
+      className={`${
+        isMobile ? "w-full px-7" : "w-1/2"
+      } flex  gap-4 flex-col justify-center items-center mx-auto`}
+    >
+      <h1 className={`${isMobile ? "text-4xl" : "text-6xl"} p-5 mt-10`}>
+        QUIZ 생성기
+      </h1>
+
+      {/* 입력 폼 */}
       <form className="flex gap-2" onSubmit={sendMessage}>
         <input
           type="text"
@@ -42,14 +72,27 @@ export default function ChatPage() {
           placeholder="주제를 입력해주세요."
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button type="submit" className="p-2 border">
-          생성
+        <button
+          type="submit"
+          className="p-1 border cursor-pointer w-28"
+          disabled={quizMutation.isPending}
+        >
+          {quizMutation.isPending ? "생성 중..." : "생성"}
         </button>
       </form>
+      <div>
+        응답 시간 : {loadingTime ? (loadingTime / 1000).toFixed(2) + "초" : "-"}
+      </div>
 
+      {/* 에러 표시 */}
+      {quizMutation.isError && (
+        <p className="text-red-500">퀴즈 생성 중 오류가 발생했습니다.</p>
+      )}
+
+      {/* 퀴즈 표시 */}
       {response && (
-        <div>
-          <div className="text-2xl p-5">
+        <div className="w-full">
+          <div className={`${isMobile ? "text-[20px]" : "text-2xl"}`}>
             <p className="mb-5">{response.question}</p>
             <ul className="flex flex-col gap-2">
               {response.options.map((opt, idx) => (
@@ -57,16 +100,21 @@ export default function ChatPage() {
                   key={idx}
                   onClick={() => setSelectAnswer(opt)}
                   className={`pl-4 cursor-pointer ${
-                    selectAnswer === opt ? "text-red-500" : "text-white"
+                    selectAnswer === opt
+                      ? "text-red-500"
+                      : "text-white hover:text-gray-500"
                   }`}
                 >{`${idx + 1}. ${opt}`}</li>
               ))}
             </ul>
           </div>
-          <div className="flex justify-end">
+          <div className="flex justify-end mt-2">
             <button
               type="button"
-              className="p-2 border bg-blue-400 cursor-pointer w-1/5"
+              className="p-2 border bg-blue-600 text-white w-1/5 rounded-lg 
+           shadow-md shadow-black/30 
+           transition hover:bg-blue-500 
+           active:scale-95 active:shadow-2xl active:shadow-black/60"
               onClick={checkAnswerClick}
             >
               제출
@@ -87,6 +135,7 @@ export default function ChatPage() {
           )}
         </div>
       )}
+      <div className="h-20"></div>
     </div>
   );
 }
