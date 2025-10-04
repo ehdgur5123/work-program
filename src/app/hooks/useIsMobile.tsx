14e90556873417cc33b81f09b9532b9b10753645
ehdgur <ehdgur3506@naver.com>
2025-10-04 23:56:50 +0900

    symbol isMobile refactoring2

--- src/app/hooks/useIsMobile.tsx
+++ src/app/hooks/useIsMobile.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function useIsMobile(breakpoint = 1024) {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
 
   useEffect(() => {
     const checkScreenSize = () => {
